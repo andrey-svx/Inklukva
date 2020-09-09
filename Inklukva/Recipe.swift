ea8aaf965ee82111a617cf3e8fe515063bc47d6b
Andrey Isaev <andron.isaev@gmail.com>
2020-09-09 18:30:33 +0500

    Added Recipe struct.

---
+++ Inklukva/Recipe.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+struct Recipe {
+    
+    var flour: Double
+    var humidity: Double
+    
+    var starter: Starter {
+        return Starter(inoculate: 0, flour: 0, water: 0)
+    }
+    
+    var dough: Dough {
+        return Dough(starter: starter.sum, flour: 0, water: 0, salt: 0)
+    }
+}
+
+struct Starter {
+    
+    var inoculate: Double
+    var flour: Double
+    var water: Double
+    
+    var sum: Double {
+        return inoculate + flour + water
+    }
+
+}
+
+struct Dough {
+    
+    var starter: Double
+    var flour: Double
+    var water: Double
+    var salt: Double
+    
+}
