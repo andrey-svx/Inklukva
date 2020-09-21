f7c4d661910318f17fdc591b4c8aa8c47fbda0be
Andrey Isaev <andron.isaev@gmail.com>
2020-09-22 02:29:30 +0500

    Added Localized file.

--- Inklukva/ViewController.swift
+++ Inklukva/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
         starterView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             starterView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            starterView.topAnchor.constraint(equalTo: view.topAnchor, constant: view.frame.height / 15),
+            starterView.topAnchor.constraint(equalTo: view.topAnchor, constant: view.frame.height / 10),
             starterView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 3/4)
         ])
     }
